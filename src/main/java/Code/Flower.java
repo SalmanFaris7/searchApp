f9d41afa497fcb724b48cf519a136cc2d44034a0
SalmanFaris7 <roxsalmanulfaris7@gmail.com>
2024-09-15 01:29:39 +0100

    Initial commit

---
+++ src/main/java/Code/Flower.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Code;
+
+/**
+ *
+ * @author Saleem Malik
+ */
+public class Flower {
+
+    private String name;
+    private String meaning;
+
+    public Flower(String name, String meaning) {
+        this.name = name;
+        this.meaning = meaning;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getMeaning() {
+        return meaning;
+    }
+
+}
